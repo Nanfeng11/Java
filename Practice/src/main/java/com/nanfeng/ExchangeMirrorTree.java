00933ebe719815fd60782b6907fe2b171f4fa7a6
Nanfeng11 <13572101760@163.com>
2019-08-19 14:07:32 +0800

    将给定二叉树转换为源二叉树的镜像

---
+++ Practice/src/main/java/com/nanfeng/ExchangeMirrorTree.java
@@ -0,0 +1,30 @@
+package com.nanfeng;
+
+/**
+ * Author：nanfeng
+ * Created:2019/8/19
+ */
+public class ExchangeMirrorTree {
+    public class TreeNode {
+        int val = 0;
+        TreeNode left = null;
+        TreeNode right = null;
+
+        public TreeNode(int val) {
+            this.val = val;
+
+        }
+
+    }
+
+    public void Mirror(TreeNode root) {
+        if (root == null){
+            return;
+        }
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        Mirror(root.left);
+        Mirror(root.right);
+    }
+}
